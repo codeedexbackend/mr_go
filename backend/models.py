83d270b3117e72a2fee85f1c178dc8d44074df74
Praveen <praveen.codeedex@gmail.com>
2024-03-15 15:21:22 +0530

    model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Add additional fields for normal sign up
+    # For example:
+    full_name = models.TextField(max_length=500, blank=True)
