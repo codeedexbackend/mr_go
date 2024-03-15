94a12be6a83ee4588bf76b4c376dda5a53b86878
Praveen <praveen.codeedex@gmail.com>
2024-03-15 15:45:45 +0530

    model2

--- backend/urls.py
+++ backend/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from backend import views 
 
 urlpatterns =[
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('login/', views.LoginView.as_view(), name='login'),
 
 ]
