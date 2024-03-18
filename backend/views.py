76dd5d9960c552b8f4f8546a41802b58533b2e1a
Prethewram <prithewramprithew1@gmail.com>
2024-03-18 12:55:30 +0530

    new

--- backend/views.py
+++ backend/views.py
@@ -1,26 +1,17 @@
 # views.py
+import datetime
+
+import jwt
 from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.exceptions import NotFound
+from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
-from .serializers import UserSerializer,UserViewSerializer,UserProfileUpdateSerializer
-from django.contrib.auth import authenticate, login
-from rest_framework.authtoken.models import Token
-from .models import CustomUser
-from rest_framework.exceptions import AuthenticationFailed
-import datetime
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.generics import RetrieveAPIView
-import jwt
-from rest_framework.exceptions import NotFound
-from rest_framework.generics import RetrieveUpdateAPIView
 
-
-
-
-
-
+from .models import CustomUser, ShippingRegistration
+from .serializers import (UserSerializer, UserViewSerializer, UserProfileUpdateSerializer, ContactusSerializer,
+                          ShippingRegSerializer, ShippingRegUpdateSerializer)
 
 
 class SignUpView(APIView):
@@ -28,9 +19,8 @@ class SignUpView(APIView):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"message": "User registered successfully",'status':True}, status=status.HTTP_201_CREATED)
+            return Response({"message": "User registered successfully", 'status': True}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 
 class LoginView(APIView):
@@ -58,13 +48,13 @@ class LoginView(APIView):
 
         response.set_cookie(key='jwt', value=token, httponly=True)
         response.data = {
-            'id':user.id,
+            'id': user.id,
             'token': token,
             'message': 'Login successful',
             'status': True
         }
         return response
-    
+
 
 class UserView(APIView):
     def get(self, request, user_id):
@@ -79,7 +69,8 @@ class UserView(APIView):
             'message': 'User Profile View',
             'status': True
         })
-    
+
+
 class UserProfileEditView(RetrieveUpdateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserProfileUpdateSerializer
@@ -102,7 +93,7 @@ class UserProfileEditView(RetrieveUpdateAPIView):
         if not obj:
             raise NotFound('User not found!')
         return obj
-    
+
 
 class LogoutView(APIView):
     def post(self, request):
@@ -113,3 +104,60 @@ class LogoutView(APIView):
             'status': True
         }
         return response
+
+
+class ContactUsView(APIView):
+    def post(self, request):
+        serializer = ContactusSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({
+            'message': 'success',
+            'status': True,
+            'user_data': serializer.data
+        }
+        )
+
+
+class ShippingRegView(APIView):
+    def post(self, request):
+        serializer = ShippingRegSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({
+            'message': 'success',
+            'status': True,
+            'user_data': serializer.data
+        })
+
+
+class ShippingReggetView(ListAPIView):
+    serializer_class = ShippingRegSerializer
+
+    def get_queryset(self):
+        user = self.kwargs['user_id']
+        return ShippingRegistration.objects.filter(user_id=user)
+
+
+class ShippingRegEditView(RetrieveUpdateAPIView):
+    queryset = ShippingRegistration.objects.all()
+    serializer_class = ShippingRegUpdateSerializer
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response({
+            'message': 'Profile updated successfully',
+            'status': True,
+            'user_data': serializer.data
+        })
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        obj = queryset.filter(id=self.kwargs.get('pk')).first()
+        if not obj:
+            raise NotFound('User not found!')
+        return obj
